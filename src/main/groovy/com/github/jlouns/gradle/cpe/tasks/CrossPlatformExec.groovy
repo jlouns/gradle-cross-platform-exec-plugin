b42b3d92e4955b461f1b8fcceb142c4f61bfd8ce
Alexander Nevskiy <kepkin@gmail.com>
2015-03-23 19:09:05 +0300

    Add extension (.bat/.sh) if corresponding file exists.

--- src/main/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExec.groovy
+++ src/main/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExec.groovy
@@ -3,6 +3,7 @@ package com.github.jlouns.gradle.cpe.tasks
 import org.gradle.api.tasks.AbstractExecTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.os.OperatingSystem
+import java.io.File
 
 /**
  * Created on 1/26/15
@@ -20,14 +21,44 @@ class CrossPlatformExec extends AbstractExecTask {
 	@Override
 	@TaskAction
 	protected void exec() {
+		List<String> commandLine = this.getCommandLine();
+		commandLine.set(0, this.findScript(commandLine.get(0)));
+		
 		if(windows) {
-			List<String> commandLine = this.getCommandLine();
 			commandLine.add(0, '/c');
 			commandLine.add(0, 'cmd');
-
-			this.setCommandLine(commandLine);
 		}
+
+		this.setCommandLine(commandLine);
 		super.exec();
+	}
+
+	private String findScript(String script) {
+		def scriptFile = new File(script)
+		if(scriptFile.isFile()) {
+			return script;
+		}
+		
+		if(windows) {
+			def batScript = new File(script + ".bat");
+			if(batScript.isFile()) {
+				return batScript;
+			}
+
+			def cmdScript = new File(script + ".cmd");
+			if(cmdScript.isFile()) {
+				return cmdScript;
+			}
+		}
+		else {
+			def shScript = new File(script + ".sh")
+			if(shScript.isFile()) {
+				return shScript;
+			}
+		}
+
+
+		return script;
 	}
 
 }
