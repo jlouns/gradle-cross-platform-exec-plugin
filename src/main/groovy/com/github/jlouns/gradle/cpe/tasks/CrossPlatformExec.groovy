f7fe410c4e3fe043d2996c11df1c07a5d9761c47
Jonathan Lounsbury <jlouns@gmail.com>
2015-04-22 13:12:44 -0400

    Adding support for resolving local command files (bat and cmd on Windows, null and sh on Unix).

--- src/main/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExec.groovy
+++ src/main/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExec.groovy
@@ -4,12 +4,14 @@ import org.gradle.api.tasks.AbstractExecTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.os.OperatingSystem
 
-/**
- * Created on 1/26/15
- *
- * @author jlouns
- */
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class CrossPlatformExec extends AbstractExecTask {
+	private static final def windowsExtensions = ['bat', 'cmd'];
+	private static final def unixExtensions = [null, 'sh'];
+
 	private boolean windows;
 
 	public CrossPlatformExec() {
@@ -20,14 +22,56 @@ class CrossPlatformExec extends AbstractExecTask {
 	@Override
 	@TaskAction
 	protected void exec() {
-		if(windows) {
-			List<String> commandLine = this.getCommandLine();
+		List<String> commandLine = this.getCommandLine();
+
+		if (!commandLine.isEmpty()) {
+			commandLine[0] = this.findCommand(commandLine[0]);
+		}
+
+		if (windows) {
 			commandLine.add(0, '/c');
 			commandLine.add(0, 'cmd');
-
-			this.setCommandLine(commandLine);
 		}
+
+		this.setCommandLine(commandLine);
+
 		super.exec();
+	}
+
+	private String findCommand(String command) {
+		if (windows) {
+			return windowsExtensions.findResult(command) { extension ->
+				String commandFile = command + '.' + extension;
+
+				if (Files.isExecutable(Paths.get(commandFile))) {
+					return commandFile;
+				}
+
+				return null;
+			};
+		} else {
+			return unixExtensions.findResult(command) { extension ->
+				Path commandFile
+				if (extension) {
+					commandFile = Paths.get(command + '.' + extension);
+				} else {
+					commandFile = Paths.get(command);
+				}
+
+				if (Files.isExecutable(commandFile)) {
+					Path cwd = Paths.get('.').toAbsolutePath().normalize();
+					String resolvedCommand = cwd.relativize(commandFile.toAbsolutePath().normalize());
+
+					if (!resolvedCommand.startsWith('.')) {
+						resolvedCommand = './' + resolvedCommand;
+					}
+
+					return resolvedCommand;
+				}
+
+				return null;
+			};
+		}
 	}
 
 }
--- src/test/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExecTest.groovy
+++ src/test/groovy/com/github/jlouns/gradle/cpe/tasks/CrossPlatformExecTest.groovy
@@ -4,6 +4,10 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * Created on 1/26/15
  *
@@ -12,40 +16,98 @@ import org.gradle.testfixtures.ProjectBuilder
 class CrossPlatformExecTest extends GroovyTestCase {
 
 	private Project project;
+	private Path[] commandFiles;
 
 	@Override
 	void setUp() {
 		project = ProjectBuilder.builder().build()
 
 		project.apply plugin: 'com.github.jlouns.cpe'
+
+		commandFiles = [
+			Paths.get('one'),
+			Paths.get('one.cmd'),
+			Paths.get('two.bat'),
+			Paths.get('two.sh')
+		];
+
+		commandFiles.each {
+			Files.createFile(it)
+		}
 	}
 
-	Task createTask() {
+	@Override
+	void tearDown() {
+		commandFiles.each {
+			Files.delete(it)
+		}
+	}
+
+	Task createTask(String executable) {
 		Task task = project.task([ 'type': CrossPlatformExec ], 'testCpe') {
-			commandLine 'echo', 'foo'
+			commandLine executable, 'foo'
 		}
 
 		try {
 			task.exec()
-		} catch (Exception ex) { }
+		} catch (Exception ex) {
+			println(ex.stackTrace)
+			// Ignore it
+		}
 
 		task
 	}
 
-	void testFormsWindowsExecCall() {
-		System.setProperty("os.name", "windows")
+	static void asOs(String os) {
+		System.setProperty('os.name', os)
+	}
 
-		Task task = createTask()
+	void testFormsLinuxExecCallForCommand() {
+		asOs('linux')
+
+		Task task = createTask('echo')
+
+		assertArrayEquals(['echo', 'foo'].toArray(), task.commandLine.toArray());
+	}
+
+	void testFormsLinuxExecCallForFile() {
+		asOs('linux')
+
+		Task task = createTask('one')
+
+		assertArrayEquals(['./one', 'foo'].toArray(), task.commandLine.toArray());
+	}
+
+	void testFormsLinuxExecCallForShFile() {
+		asOs('linux')
+
+		Task task = createTask('two')
+
+		assertArrayEquals(['./two.sh', 'foo'].toArray(), task.commandLine.toArray());
+	}
+
+	void testFormsWindowsExecCallForCommand() {
+		asOs('windows')
+
+		Task task = createTask('echo')
 
 		assertArrayEquals(['cmd', '/c', 'echo', 'foo'].toArray(), task.commandLine.toArray());
 	}
 
-	void testFormsLinuxExecCall() {
-		System.setProperty("os.name", "linux")
+	void testFormsWindowsExecCallForCmdFile() {
+		asOs('windows')
 
-		Task task = createTask()
+		Task task = createTask('one')
 
-		assertArrayEquals(['echo', 'foo'].toArray(), task.commandLine.toArray());
+		assertArrayEquals(['cmd', '/c', 'one.cmd', 'foo'].toArray(), task.commandLine.toArray());
+	}
+
+	void testFormsWindowsExecCallForBatFile() {
+		asOs('windows')
+
+		Task task = createTask('two')
+
+		assertArrayEquals(['cmd', '/c', 'two.bat', 'foo'].toArray(), task.commandLine.toArray());
 	}
 
 }
